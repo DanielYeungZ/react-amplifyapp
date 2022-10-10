19c15f4ff62d713a4b1adce1251a174eea2b503c
daniel <daniel@famepick.com>
2022-10-09 21:03:21 -0700

    trigger deployment

--- amplify/backend/function/stream/src/index.js
+++ amplify/backend/function/stream/src/index.js
@@ -2,7 +2,6 @@ const AWS = require("aws-sdk");
 const ddb = new AWS.DynamoDB.DocumentClient({region: "us-east-1"});
 const orderARN = "arn:aws:dynamodb:us-east-1:659623045881:table/Order-sutffwtm3fa5hbynk5wwjbt5we-staging/stream/2022-10-08T20:19:16.786"
 const productTable = "roduct-sutffwtm3fa5hbynk5wwjbt5we-stagin"
-const {parseDynamoStreamRecord} = require("dynamo-stream-record-parser");
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
@@ -10,24 +9,19 @@ exports.handler = async (event) => {
     console.log(`EVENT: ${JSON.stringify(event)}`);
 
     for (let record of event.Records) {
-        console.log(record.eventID);
+        console.log(record);
         console.log(record.eventName);
         console.log('DynamoDB Record: %j', record.dynamodb);
 
 
         if (record.eventName === "MODIFY" && record.eventSourceARN === orderARN) {
-            const parsedStreamRecord = parseDynamoStreamRecord(record);
-            console.log('parsedStreamRecord: ', parsedStreamRecord)
-            let newRecord = parsedStreamRecord.dynamodb.NewImage;
-            let oldRecord = parsedStreamRecord.dynamodb.OldImage;
-            if (newRecord.status === 'DELIVERED' && oldRecord.status !== 'DELIVERED') {
+            let newRecord = record.dynamodb.NewImage;
+            let oldRecord = record.dynamodb.OldImage;
+            if (newRecord.status && newRecord.status.S === 'DELIVERED' && oldRecord.status  &&oldRecord.status.S!== 'DELIVERED') {
                 const params = {
-                    TableName: process.env.TALENT_CAMPAIGN_TABLE,
+                    TableName: productTable,
                     Key: {id: newRecord.productID},
                 };
-
-                console.log(`[models/campaign] getById: ${JSON.stringify(params, null, 2)}`);
-
                 const product = await ddb.get(params).promise();
                 console.log("Product : ", product.Item)
 
@@ -38,13 +32,12 @@ exports.handler = async (event) => {
                         id: newRecord.id,
                     },
                     UpdateExpression: "set quantity = :quantity",
-                    // This expression is what updates the item attribute
                     ExpressionAttributeValues: {
                         ":quantity": 100,
                     },
                     ReturnValues: "UPDATED_NEW",
                 };
-                let order = await ddb.update(updateParams).promise();
+                // let order = await ddb.update(updateParams).promise();
             }
         }
 
